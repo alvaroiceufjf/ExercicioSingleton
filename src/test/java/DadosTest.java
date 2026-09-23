242f70292a4f2b4f9f63721c00b7da9028205ee5
alvaroiceufjf <alvaro.domingues@estudante.ufjf.br>
2026-09-23 19:59:06 -0300

    Copia Projeto Exemplo

---
+++ src/test/java/DadosTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DadosTest {
+
+    @Test
+    public void deveRetornarNomeEscola() {
+        Parametros.getInstance().setNomeEscola("Escola 1");
+        assertEquals("Escola 1", Parametros.getInstance().getNomeEscola());
+    }
+
+    @Test
+    public void deveRetornarUsuarioLogado() {
+        Parametros.getInstance().setUsuarioLogado("Usuario 1");
+        assertEquals("Usuario 1", Parametros.getInstance().getUsuarioLogado());
+    }
+
+}
